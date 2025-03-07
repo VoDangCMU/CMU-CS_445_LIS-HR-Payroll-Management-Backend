2a3f2573f08228e12b3297d50afb9d1c355aa768
NGUYEN BA KHOA <khoa.nguyenba@outlook.com>
2025-03-07 14:15:00 +0700

    savework

--- src/datasources/payroll/entity/payroll.entity.ts
+++ src/datasources/payroll/entity/payroll.entity.ts
@@ -1,6 +1,5 @@
-import {Entity, Column, ObjectIdColumn} from "typeorm"
-import {ObjectId} from "mongodb";
-
+import {Column, Entity, ObjectIdColumn} from "typeorm"
+import {ObjectId} from "bson";
 
 @Entity('payroll_records')
 export class PayrollRecord {
@@ -10,18 +9,18 @@ export class PayrollRecord {
     @Column()
     employeeID: number;
 
-    @Column({ type: 'date' })
+    @Column({type: 'date'})
     payDate: Date;
 
-    @Column({ type: 'bigint' })
+    @Column({type: 'bigint'})
     grossSalary: number;
 
-    @Column({ type: 'bigint' })
+    @Column({type: 'bigint'})
     netSalary: number;
 
-    @Column({ type: 'bigint' })
+    @Column({type: 'bigint'})
     deductions: number;
 
-    @Column({ type: 'bigint' })
+    @Column({type: 'bigint'})
     benefitsPaid: number;
 }
